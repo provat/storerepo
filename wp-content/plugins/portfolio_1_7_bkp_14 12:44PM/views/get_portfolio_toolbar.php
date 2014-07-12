603029d5549eba52eac89dd550f44c2e86d6428a
provat <provat.cn@gmail.com>
2014-07-12 12:14:13 +0530

    all files deleted

--- wp-content/plugins/portfolio_1_7_bkp_14 12:44PM/views/get_portfolio_toolbar.php
+++
@@ -1,4 +0,0 @@
-<div class="project_toolbar" align="center">
-<input type="button" name="save_project"  value="Save" id="save_project" class="save_btn m_right10"> 
-<input type="button" name="continue" value="Continue" class="save_btn m_right10" id="continu_project">
-</div>
